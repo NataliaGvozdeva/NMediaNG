a7735db820621a05f46d1e50ee3fe6a5957ccbb3
Natalia Gvozdeva (intern) <gvozdeva-natalia@mail.ru>
2022-06-21 00:14:53 +0300

    2.2.MVVM

--- app/src/main/java/ru/netology/nmediang/dto/Post.kt
+++ app/src/main/java/ru/netology/nmediang/dto/Post.kt
@@ -5,7 +5,7 @@ data class Post(
     val author: String,
     val content: String,
     val published: String,
-    var likedByMe: Boolean,
+    val likedByMe: Boolean,
     var likesSum: Long,
     var sharedSum: Long,
     var viewSum: Long
